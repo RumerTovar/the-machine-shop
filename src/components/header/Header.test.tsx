cd6b35d6742421cd07d0331e7e0cbd2679470239
Rumer Tovar <rumertovar7474@gmail.com>
2023-05-22 20:15:15 -0500

    approved tests

--- src/components/header/Header.test.tsx
+++ src/components/header/Header.test.tsx
@@ -11,7 +11,7 @@ describe('Header', () => {
 
  it('rendering contact number', () => {
   render(<Header />);
-  const contact = screen.getByText('+54 0800 222 22999');
+  const contact = screen.getByText(/\+54 0800 222 2299/i);
 
   expect(contact).toBeInTheDocument();
  });
