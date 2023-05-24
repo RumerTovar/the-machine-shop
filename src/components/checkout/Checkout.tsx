0be9e395d0dc378eccffb0b25874d3a01030a3a0
Rumer Tovar <rumertovar7474@gmail.com>
2023-05-24 13:12:45 -0500

    mobile checkout layout

--- src/components/checkout/Checkout.tsx
+++ src/components/checkout/Checkout.tsx
@@ -29,191 +29,207 @@ export default function Checkout() {
    <Link href='/' className={styles.title}>
     The Machine Shop
    </Link>
-   <main>
-    <section className={styles.summaryHeader}>
-     <div className={styles.summaryHeaderContent}>
-      <div className={styles.leftHeader}>
-       <span className={`material-symbols-outlined ${styles.cartIcon}`}>
-        shopping_cart
-       </span>
-       <p>Mostrar resumen del pedido</p>
-      </div>
-      <NumericFormat
-       value={cart.reduce(
-        (acc, item) => acc + item.product_price * item.product_quantity,
-        0
-       )}
-       decimalScale={2}
-       thousandSeparator='.'
-       fixedDecimalScale
-       decimalSeparator=','
-       displayType='text'
-       prefix={'$'}
-       renderText={(value) => <p className={styles.price}>{value}</p>}
-      />
+   <section className={styles.summaryHeader}>
+    <div className={styles.summaryHeaderContent}>
+     <div className={styles.leftHeader}>
+      <span className={`material-symbols-outlined ${styles.cartIcon}`}>
+       shopping_cart
+      </span>
+      <p>Mostrar resumen del pedido</p>
+     </div>
+     <NumericFormat
+      value={cart.reduce(
+       (acc, item) => acc + item.product_price * item.product_quantity,
+       0
+      )}
+      decimalScale={2}
+      thousandSeparator='.'
+      fixedDecimalScale
+      decimalSeparator=','
+      displayType='text'
+      prefix={'$'}
+      renderText={(value) => <p className={styles.price}>{value}</p>}
+     />
+    </div>
+   </section>
+   <div className={styles.orderSummary}>
+    <section className={styles.productList}>
+     <div className={styles.productListContent}>
+      {cart.map((product) => {
+       const {
+        id_product,
+        product_image_url,
+        product_name,
+        product_price,
+        product_quantity,
+       } = product;
+       return (
+        <div className={styles.productContainer}>
+         <div className={styles.productLeft}>
+          <Image
+           className={styles.productImage}
+           width={64}
+           height={64}
+           alt={product_name}
+           src={product_image_url}
+          />
+          <p className={styles.productName}>{product_name}</p>
+         </div>
+
+         <NumericFormat
+          value={product_price}
+          decimalScale={2}
+          thousandSeparator='.'
+          fixedDecimalScale
+          decimalSeparator=','
+          displayType='text'
+          prefix={'$'}
+          renderText={(value) => <p className={styles.price}>{value}</p>}
+         />
+        </div>
+       );
+      })}
      </div>
     </section>
-    <div className={styles.orderSummary}>
-     <section className={styles.productList}>
-      <div className={styles.productListContent}>
-       {cart.map((product) => {
-        const {
-         id_product,
-         product_image_url,
-         product_name,
-         product_price,
-         product_quantity,
-        } = product;
-        return (
-         <div className={styles.productContainer}>
-          <div className={styles.productLeft}>
-           <Image
-            className={styles.productImage}
-            width={64}
-            height={64}
-            alt={product_name}
-            src={product_image_url}
-           />
-           <p className={styles.productName}>{product_name}</p>
-          </div>
-
-          <NumericFormat
-           value={product_price}
-           decimalScale={2}
-           thousandSeparator='.'
-           fixedDecimalScale
-           decimalSeparator=','
-           displayType='text'
-           prefix={'$'}
-           renderText={(value) => <p className={styles.price}>{value}</p>}
-          />
-         </div>
-        );
-       })}
-      </div>
-     </section>
-     <section className={styles.discountCode}>
-      <div className={styles.discountCodeContent}>
-       <input
-        type='text'
-        placeholder='Código de descuento'
-        className={styles.inputDiscount}
-       />
-       <button className={`${styles.applyButton} material-symbols-outlined`}>
-        arrow_forward
-       </button>
-      </div>
-     </section>
-     <section className={styles.totalSection}>
-      <div className={styles.totalSectionContent}>
-       <div className={styles.subTotal}>
-        <p>Subtotal</p>
-        <NumericFormat
-         value={cart.reduce(
-          (acc, item) => acc + item.product_price * item.product_quantity,
-          0
-         )}
-         decimalScale={2}
-         thousandSeparator='.'
-         fixedDecimalScale
-         decimalSeparator=','
-         displayType='text'
-         prefix={'$'}
-         renderText={(value) => <p className={styles.price}>{value}</p>}
-        />
-       </div>
-       <div className={styles.shipping}>
-        <p>Shipping</p>
-        <p>Calculado en el siguiente paso</p>
-       </div>
-       <div className={styles.total}>
-        <p>Total</p>
-        <NumericFormat
-         value={cart.reduce(
-          (acc, item) => acc + item.product_price * item.product_quantity,
-          0
-         )}
-         decimalScale={2}
-         thousandSeparator='.'
-         fixedDecimalScale
-         decimalSeparator=','
-         displayType='text'
-         prefix={'$'}
-         renderText={(value) => <p className={styles.price}> {value}</p>}
-        />
-       </div>
-      </div>
-     </section>
-    </div>
-    <div className={styles.contactTitle}>
-     <p>Contacto</p>
-     <p>
-      ¿Ya tienes una cuenta? <Link href='/'>Inicia sesion </Link>
-     </p>
-    </div>
-    <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-     <div className={styles.formEmailGroup}>
-      <label className={styles.label}>Email</label>
-      <input className={styles.input} />
-     </div>
-     <div className={styles.formSubscribe}>
-      <input type='checkbox' name='subscribe' />
-      <label className='subscribe'>
-       Quiero recibir por email noticias y ofertas
-      </label>
-     </div>
-     <p className={styles.shippingAddress}>Dirección de envío</p>
-     <div className={styles.inputGroup}>
-      <label className={styles.label}>Pais/Region</label>
-      <select className={`${styles.inputPadding} ${styles.input}`}>
-       <option value=''>USA</option>
-      </select>
-     </div>
-     <div className={styles.inputGroup}>
-      <label htmlFor='' className={styles.label}>
-       Primer nombre
-      </label>
+    <section className={styles.discountCode}>
+     <div className={styles.discountCodeContent}>
       <input
        type='text'
-       name='firstName'
-       placeholder=''
-       className={styles.input}
+       placeholder='Código de descuento'
+       className={styles.inputDiscount}
       />
+      <button className={`${styles.applyButton} material-symbols-outlined`}>
+       arrow_forward
+      </button>
      </div>
-     <div className={styles.inputGroup}>
-      <label htmlFor='' className={styles.label}>
-       Primer apellido
-      </label>
-      <input type='text' name='lastName' className={styles.input} />
+    </section>
+    <section className={styles.totalSection}>
+     <div className={styles.totalSectionContent}>
+      <div className={styles.subTotal}>
+       <p>Subtotal</p>
+       <NumericFormat
+        value={cart.reduce(
+         (acc, item) => acc + item.product_price * item.product_quantity,
+         0
+        )}
+        decimalScale={2}
+        thousandSeparator='.'
+        fixedDecimalScale
+        decimalSeparator=','
+        displayType='text'
+        prefix={'$'}
+        renderText={(value) => <p className={styles.price}>{value}</p>}
+       />
+      </div>
+      <div className={styles.shipping}>
+       <p>Shipping</p>
+       <p>Calculado en el siguiente paso</p>
+      </div>
+      <div className={styles.total}>
+       <p>Total</p>
+       <NumericFormat
+        value={cart.reduce(
+         (acc, item) => acc + item.product_price * item.product_quantity,
+         0
+        )}
+        decimalScale={2}
+        thousandSeparator='.'
+        fixedDecimalScale
+        decimalSeparator=','
+        displayType='text'
+        prefix={'$'}
+        renderText={(value) => <p className={styles.price}> {value}</p>}
+       />
+      </div>
      </div>
-     <div className={styles.inputGroup}>
-      <label htmlFor='' className={styles.label}>
-       Dirección
-      </label>
-      <input type='text' name='address' className={styles.input} />
-     </div>
-     <div className={styles.inputGroup}>
-      <label htmlFor='' className={styles.label}>
-       Apartamento, suitem etc. (opcional)
-      </label>
-      <input type='text' name='apartment' className={styles.input} />
-     </div>
-     <div className={styles.inputGroup}>
-      <label htmlFor='' className={styles.label}>
-       Ciudad
-      </label>
-      <input type='text' name='city' className={styles.input} />
-     </div>
+    </section>
+   </div>
+   <div className={styles.contactTitle}>
+    <p>Contacto</p>
+    <p>
+     ¿Ya tienes una cuenta? <Link href='/'>Inicia sesion </Link>
+    </p>
+   </div>
+   <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+    <div className={styles.formEmailGroup}>
+     <label className={styles.label}>Email</label>
+     <input className={styles.input} />
+    </div>
+    <div className={styles.formSubscribe}>
+     <input type='checkbox' name='subscribe' />
+     <label htmlFor='subscribe' className='subscribe'>
+      Quiero recibir por email noticias y ofertas
+     </label>
+    </div>
+    <p className={styles.shippingAddress}>Dirección de envío</p>
 
-     <select name='' id='' className={styles.input}>
-      <option value=''>Buenos aires</option>
+    <div className={styles.inputGroup}>
+     <label htmlFor='firstName' className={styles.label}>
+      Primer nombre
+     </label>
+     <input type='text' name='firstName' className={styles.input} />
+    </div>
+    <div className={styles.inputGroup}>
+     <label htmlFor='lastName' className={styles.label}>
+      Primer apellido
+     </label>
+     <input type='text' name='lastName' className={styles.input} />
+    </div>
+    <div className={styles.inputGroup}>
+     <label htmlFor='address' className={styles.label}>
+      Dirección
+     </label>
+     <input type='text' name='address' className={styles.input} />
+    </div>
+    <div className={styles.inputGroup}>
+     <label htmlFor='apartment' className={styles.label}>
+      Apartamento, suitem etc. (opcional)
+     </label>
+     <input type='text' name='apartment' className={styles.input} />
+    </div>
+    <div className={styles.inputGroup}>
+     <label className={styles.label}>Provincia</label>
+     <select className={`${styles.inputPadding} ${styles.input}`}>
+      <option value=''>USA</option>
      </select>
-     <input type='text' name='zipCode' id='' className={styles.input} />
-     <input type='text' name='phone' id='' className={styles.input} />
-     <button type='submit'>Continue to shipping</button>
-     <Link href='/'>Return to cart</Link>
-    </form>
-   </main>
+    </div>
+    <div className={styles.inputGroup}>
+     <label className={styles.label}>Ciudad</label>
+     <select className={`${styles.inputPadding} ${styles.input}`}>
+      <option value=''>USA</option>
+     </select>
+    </div>
+    <div className={styles.inputGroup}>
+     <label htmlFor='apartment' className={styles.label}>
+      Telefono
+     </label>
+     <input type='text' name='apartment' className={styles.input} />
+    </div>
+    <button className={styles.submit} type='submit'>
+     Continue to shipping
+    </button>
+    <Link href='/' className={styles.return}>
+     <span className={`${styles.backArrow} material-symbols-outlined`}>
+      arrow_back_ios
+     </span>
+     Return to cart
+    </Link>
+   </form>
+   <hr />
+   <footer className={styles.footer}>
+    <Link className={styles.footerLinks} href='/'>
+     Politica de devolucion
+    </Link>
+    <Link className={styles.footerLinks} href='/'>
+     Politica de envio
+    </Link>
+    <Link className={styles.footerLinks} href='/'>
+     Politica de privacidad
+    </Link>
+    <Link className={styles.footerLinks} href='/'>
+     Terminos y servicios
+    </Link>
+   </footer>
   </div>
  );
 }
