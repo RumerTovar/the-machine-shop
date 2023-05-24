54e98d0636ae086758ceb56f1ef42ac0ba002cb3
Rumer Tovar <rumertovar7474@gmail.com>
2023-05-24 11:56:17 -0500

    sideMenu tests

--- src/components/sideMenu/SideMenu.test.tsx
+++ src/components/sideMenu/SideMenu.test.tsx
@@ -2,12 +2,50 @@ import { render, screen } from '@testing-library/react';
 import SideMenu from './SideMenu';
 
 describe('SideMenu', () => {
- it('inicio button', () => {
+ it('render start link', () => {
   render(<SideMenu />);
-  const home = screen.getByRole('link', {
+  const link = screen.getByRole('link', {
    name: /home inicio/i,
   });
 
-  expect(home).toBeInTheDocument();
+  expect(link).toBeInTheDocument();
+ });
+
+ it('render my profile link', () => {
+  render(<SideMenu />);
+
+  const link = screen.getByRole('link', {
+   name: /account_circle mi perfil/i,
+  });
+
+  expect(link).toBeInTheDocument();
+ });
+
+ it('render cart link', () => {
+  render(<SideMenu />);
+
+  const link = screen.getByRole('link', {
+   name: /shopping_cart carrito/i,
+  });
+
+  expect(link).toBeInTheDocument();
+ });
+
+ it('render my shoppings link', () => {
+  render(<SideMenu />);
+
+  const link = screen.getByRole('link', {
+   name: /shopping_bag mis compras/i,
+  });
+
+  expect(link).toBeInTheDocument();
+ });
+
+ it('render privacy Policy link', () => {
+  render(<SideMenu />);
+
+  const link = screen.getByText(/politicas de privacidad/i);
+
+  expect(link).toBeInTheDocument();
  });
 });
